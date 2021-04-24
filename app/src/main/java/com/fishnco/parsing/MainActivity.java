0bcb723741036fa4f5b4d2e9a17c69903f002ade
junyi <collau@hotmail.com>
2021-04-24 17:47:58 +0800

    2. Setting up JSONArray Android Volley Request

--- app/src/main/java/com/fishnco/parsing/MainActivity.java
+++ app/src/main/java/com/fishnco/parsing/MainActivity.java
@@ -9,20 +9,48 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
+    RequestQueue queue;
     String url = "https://www.google.com";
+    String apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("JSON", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("JSON", "onCreate: Failed");
+
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+
+        getString(queue);
+
+    }
+
+    private void getString(RequestQueue queue) {
         // Create request for String
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             // Display the contents of our url
