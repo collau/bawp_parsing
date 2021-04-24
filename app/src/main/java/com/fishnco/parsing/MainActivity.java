f20d6fea6e3a97dafec62d607818ef90b6a6a6ed
junyi <collau@hotmail.com>
2021-04-24 20:42:51 +0800

    4. Parsing a JSON Payload with the JsonObject Request

--- app/src/main/java/com/fishnco/parsing/MainActivity.java
+++ app/src/main/java/com/fishnco/parsing/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,14 +24,43 @@ public class MainActivity extends AppCompatActivity {
     RequestQueue queue;
     String url = "https://www.google.com";
     String apiUrl = "https://jsonplaceholder.typicode.com/todos";
+    String jsonObjUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView textView = findViewById(R.id.textView);
+
         queue = Volley.newRequestQueue(this);
 
+//        getJSONArrayRequest();
+
+//        getString(queue);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, jsonObjUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    textView.setText(response.getString("title"));
+                    Log.d("JSONObject", response.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("MainActivity", "Error retrieving JSONObject");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("JSON", "onCreate JSONObject: Failed");
+            }
+        });
+        queue.add(jsonObjectRequest);
+
+    }
+
+    private void getJSONArrayRequest() {
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
@@ -56,9 +86,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         queue.add(jsonArrayRequest);
-
-        getString(queue);
-
     }
 
     private void getString(RequestQueue queue) {
