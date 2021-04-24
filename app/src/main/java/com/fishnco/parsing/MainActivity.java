8b31b1415eb4e49dc16e59501fa70c8f290e4f6a
junyi <collau@hotmail.com>
2021-04-24 17:28:25 +0800

    1. Setting up the Volley Network Library and Parsing a String

--- app/src/main/java/com/fishnco/parsing/MainActivity.java
+++ app/src/main/java/com/fishnco/parsing/MainActivity.java
@@ -3,12 +3,43 @@ package com.fishnco.parsing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url = "https://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Create request for String
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            // Display the contents of our url
+            @Override
+            public void onResponse(String response) {
+                Log.d("Main", "onCreate: " + response.substring(0, 500));
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Main", "Failed to get info");
+
+            }
+        });
+
+        // Add the request to the request queue
+        queue.add(stringRequest);
     }
 }
